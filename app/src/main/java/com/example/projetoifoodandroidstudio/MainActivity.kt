415b9df99e71fe42a4873865b2ad59960b0dc1ec
JOAO LUCAS SORDA DE ALMEIDA <jlucas@cs.up.edu.br>
2025-09-11 09:08:53 -0300

    Projeto Criado no Android Studio

---
+++ app/src/main/java/com/example/projetoifoodandroidstudio/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.projetoifoodandroidstudio
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projetoifoodandroidstudio.ui.theme.ProjetoIfoodAndroidStudioTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ProjetoIfoodAndroidStudioTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ProjetoIfoodAndroidStudioTheme {
+        Greeting("Android")
+    }
+}
