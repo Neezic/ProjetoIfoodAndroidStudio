1f73d548f78f969c834788e6cc483a4183822c2b
dudu025 <dudueduardorodrigo@gmail.com>
2025-09-18 09:16:45 -0300

    pequenas atualizacoes no codigo

--- app/src/main/java/com/example/projetoifoodandroidstudio/TelaLogin.kt
+++ app/src/main/java/com/example/projetoifoodandroidstudio/TelaLogin.kt
@@ -1,28 +1,24 @@
 package com.example.projetoifoodandroidstudio
 
-
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.graphics.Color
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.projetoifoodandroidstudio.ui.theme.IfoodRed
-import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-
+import com.example.projetoifoodandroidstudio.ui.theme.IfoodRed
 
 @Composable
 fun TelaLogin(
@@ -31,57 +27,75 @@ fun TelaLogin(
     onLoginSucess: () -> Unit
 ) {
     val loginSuccess by viewModel.loginSucess.collectAsStateWithLifecycle()
-    if(loginSuccess){
+    val context = LocalContext.current
+
+    if (loginSuccess) {
         LaunchedEffect(Unit) {
             onLoginSucess()
             viewModel.onLoginHandled()
         }
     }
-    Column(
-        modifier = modifier
-        .fillMaxSize()
-        .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Bem-Vindo!",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold
-        )
-        Spacer(modifier = Modifier.height(32.dp))
-        OutlinedTextField(
-            value = viewModel.email,
-            onValueChange = {viewModel.onEmailChange(it)},
-            label = {Text("email")},
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(
-            value = viewModel.senha,
-            onValueChange = {viewModel.onSenhaChange(it)},
-            label = {Text("senha")},
-            modifier = Modifier.fillMaxWidth(),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            visualTransformation = PasswordVisualTransformation()
-        )
-        if (viewModel.mensagemErro != null) {
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = viewModel.mensagemErro!!,
-                color = IfoodRed,
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
 
-        Spacer(modifier = Modifier.height(32.dp))
-        Button(
-            onClick = {viewModel.fazerLogin()},
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(containerColor = IfoodRed)
+
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = Color.White
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("Entrar", color = Color.White)
+            Text(
+                text = "Bem-Vindo!",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            OutlinedTextField(
+                value = viewModel.email,
+                onValueChange = { viewModel.onEmailChange(it) },
+                label = { Text("email") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            OutlinedTextField(
+                value = viewModel.senha,
+                onValueChange = { viewModel.onSenhaChange(it) },
+                label = { Text("senha") },
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+
+            if (viewModel.mensagemErro != null) {
+                LaunchedEffect(viewModel.mensagemErro) {
+                    Toast.makeText(
+                        context,
+                        viewModel.mensagemErro,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = viewModel.mensagemErro!!,
+                    color = IfoodRed,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(
+                onClick = { viewModel.fazerLogin() },
+                modifier = Modifier.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(containerColor = IfoodRed)
+            ) {
+                Text("Entrar", color = Color.White)
+            }
         }
     }
 }
